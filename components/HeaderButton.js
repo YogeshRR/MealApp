631ad78b9594a29c8849fe8e231b375855b93f84
Yogesh Raut <yogesh@Yogeshs-MacBook-Air.local>
2024-02-20 18:16:17 +0530

    108 Adding an Icon Button to a Header

--- components/HeaderButton.js
+++ components/HeaderButton.js
@@ -1,9 +1,24 @@
-import { Button } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 function HeaderButton() {
   function likeButtonPressed() {
     console.log("pressed");
   }
-  return <Button title="Tap Me" onPress={likeButtonPressed} />;
+  return (
+    <Pressable
+      onPress={likeButtonPressed}
+      style={({ pressed }) => pressed && styles.pressable}
+    >
+      <Ionicons name="star" size={24} color={"white"} />
+    </Pressable>
+  );
 }
 export default HeaderButton;
+
+const styles = StyleSheet.create({
+  pressable: {
+    opacity: 0.75,
+    marginHorizontal: 10,
+  },
+});
