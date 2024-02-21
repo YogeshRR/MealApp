2edb508bd81fcd9aec0991e79bfa74700e5452f5
Yogesh Raut <yogesh@Yogeshs-MacBook-Air.local>
2024-02-21 12:52:04 +0530

    116 Getting Started with React's Context API

--- store/context/favorite-context.js
+++ store/context/favorite-context.js
@@ -1,12 +1,32 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const FavoritesContext = createContext({
-  id: [],
+  ids: [],
   addFavorite: () => {},
   removeFavorite: () => {},
 });
 
 function FavoritesContextProvider({ children }) {
-  return <FavoritesContext.Provider>{children}</FavoritesContext.Provider>;
+  const [favoriteMealId, setFavoriteMealId] = useState([]);
+  function addFavorite(id) {
+    setFavoriteMealId((curFavMealId) => [...curFavMealId, id]);
+  }
+
+  function removeFavorite(id) {
+    setFavoriteMealId((curFavMealId) =>
+      curFavMealId.filter((mealId) => mealId !== id)
+    );
+  }
+  const value = {
+    ids: favoriteMealId,
+    addFavorite: addFavorite,
+    removeFavorite: removeFavorite,
+  };
+  return (
+    <FavoritesContext.Provider value={value}>
+      {children}
+    </FavoritesContext.Provider>
+  );
 }
+
 export default FavoritesContextProvider;
